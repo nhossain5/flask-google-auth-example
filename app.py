4147a19a79de5414914f376ed8569f338fcaa2ce
Nahid Hossain <mdnhossain11@gmail.com>
2022-04-04 06:46:12 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,106 @@
+import os
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
+from flask_dance.consumer import oauth_authorized
+from flask_dance.contrib.google import make_google_blueprint, google
+from flask_dance.consumer.storage.sqla import OAuthConsumerMixin, SQLAlchemyStorage
+
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+path = os.path.dirname(os.path.realpath(__file__))
+db_path = os.path.join(path, "app.db")
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_path
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "YOUR-SECRET-HERE"
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "google.login"
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), unique=True)
+    name = db.Column(db.String(256))
+
+
+class OAuth(OAuthConsumerMixin, db.Model):
+    provider_user_id = db.Column(db.String(256), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship(User)
+
+
+db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+google_blueprint = make_google_blueprint(
+    client_id="943068681276-s5jsp0eek49mua8lho0n1nst7atmcleh.apps.googleusercontent.com",
+    client_secret="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",
+    scope=[
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "openid",
+    ],
+    offline=True,
+    reprompt_consent=True,
+    storage=SQLAlchemyStorage(OAuth, db.session, user=current_user),
+)
+
+app.register_blueprint(google_blueprint)
+
+
+@app.route("/")
+def index():
+    google_data = None
+    user_info_endpoint = "oauth2/v2/userinfo"
+    if current_user.is_authenticated and google.authorized:
+        google_data = google.get(user_info_endpoint).json()
+    return render_template(
+        "index.j2",
+        google_data=google_data,
+        fetch_url=google.base_url + user_info_endpoint,
+    )
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+@oauth_authorized.connect_via(google_blueprint)
+def google_logged_in(blueprint, token):
+    resp = blueprint.session.get("/oauth2/v2/userinfo")
+    user_info = resp.json()
+    user_id = str(user_info["id"])
+    oauth = OAuth.query.filter_by(
+        provider=blueprint.name, provider_user_id=user_id
+    ).first()
+    if not oauth:
+        oauth = OAuth(provider=blueprint.name, provider_user_id=user_id, token=token)
+    else:
+        oauth.token = token
+        db.session.add(oauth)
+        db.session.commit()
+        login_user(oauth.user)
+    if not oauth.user:
+        user = User(email=user_info["email"], name=user_info["name"])
+        oauth.user = user
+        db.session.add_all([user, oauth])
+        db.session.commit()
+        login_user(user)
+
+    return False
+
+
+app.run(debug=True)
---
+++ run.py
@@ -0,0 +1,8 @@
+from app import app, db
+
+
+@app.cli.command(help="Initialise the database.")
+def initdb():
+    db.drop_all()
+    db.create_all()
+    print("Done.")
